68c24c1251ce6b4dacf37b024c8d892941d21cbe
AndyLinxies <linxies@gmail.com>
2021-06-08 16:11:16 +0200

    Exo5

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./header.js"; //on importe la fonction Header
+function App() {
+  
+
+  return (
+    <div className="app">
+      <Header /> {/*On lance la fonction Header */}
+    </div>
+  );
+}
+
+export default App;
